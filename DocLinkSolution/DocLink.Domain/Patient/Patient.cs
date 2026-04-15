b7fdbfe55811be9e47b6863c9e9b053f30f2e48c
Diconcili <eduardodiconcilicosta@yahoo.com.br>
2026-04-15 19:28:40 -0300

    domain implemented, such as its layer of Domain and Shared

---
+++ DocLinkSolution/DocLink.Domain/Patient/Patient.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DocLink.Domain.Shared;
+
+namespace DocLink.Domain.Patient
+{
+    internal class Patient : Entity
+    {
+        public PatientId Id { get; }
+        public PatientFullName PatientName { get; private set; }
+
+        public Patient(PatientId id, PatientFullName patientName)
+        {
+            Id = id;
+            PatientName = patientName;
+        }
+
+        public override object GetId() => Id;
+    }
+}
---
+++ DocLinkSolution/DocLink.Domain/Shared/Entity.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DocLink.Domain.Shared
+{
+    public abstract class Entity
+    {
+        public abstract object GetId();
+
+        public override bool Equals(object? obj)
+        {
+            if(obj is not Entity other)
+            {
+                return false;
+            }
+            if (ReferenceEquals(this, other))
+            {
+                return true;
+            }
+            if (GetType() != other.GetType())
+            {
+                return false;
+            }
+
+            return GetId().Equals(other.GetId());
+        }
+
+        public override int GetHashCode() => GetId().GetHashCode();
+    }
+}
