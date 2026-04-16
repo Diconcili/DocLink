556ae40d54ffb3490586374e38e52968486ca83c
Eduardo DIconcili <eduardodiconcilicosta@yahoo.com.br>
2026-04-16 16:42:19 -0300

    PatientDto implemented

---
+++ DocLinkSolution/Application/Patient/DTOs/PatientDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Patient.DTOs
+{
+    public sealed class PatientDto
+    {
+        public Guid Id { get; init; }
+        public string FullName { get; init; }
+
+    }
+}
