b36f2773e5103a3abf68816b39b0996c16a5aa45
Eduardo DIconcili <eduardodiconcilicosta@yahoo.com.br>
2026-04-16 16:40:40 -0300

    IPdfParser implemented

---
+++ DocLinkSolution/Application/Interfaces/IPdfParser.cs
@@ -0,0 +1,12 @@
+﻿using DocLink.Domain.Record;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Interfaces
+{
+    public interface IPdfParser
+    {
+        PdfMetadata Parse(string filePath);
+    }
+}
