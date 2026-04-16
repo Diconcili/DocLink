294459a6561e1db1ac3dbe7e7a33eab73ce02aaf
Eduardo DIconcili <eduardodiconcilicosta@yahoo.com.br>
2026-04-16 19:19:23 -0300

    Implemented DTOs for record

---
+++ DocLinkSolution/Application/Record/DTOs/DocumentDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Record.DTOs
+{
+    internal class DocumentDto
+    {
+        public Guid Id { get; init; }
+        public string FileName { get; init; }
+        public DateTime AddedAt { get; init; }
+    }
+}
---
+++ DocLinkSolution/Application/Record/DTOs/RecordDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Record.DTOs
+{
+    internal class RecordDto
+    {
+        public Guid Id { get; init; }
+        public Guid PatientId { get; init; }
+
+        public IEnumerable<DocumentDto> Documents { get; init; } = Enumerable.Empty<DocumentDto>();
+    }
+}
