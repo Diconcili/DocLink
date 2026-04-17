bf6982fec09e6b5e8390261572ac5f2a35c1f7e5
Eduardo DIconcili <eduardodiconcilicosta@yahoo.com.br>
2026-04-17 15:55:57 -0300

    CreateRecordCommand implemented

---
+++ DocLinkSolution/Application/Record/Commands/CreateRecordCommand.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Record.Commands
+{
+    public sealed class CreateRecordCommand
+    {
+        public Guid PatientId { get; init; }
+    }
+}
