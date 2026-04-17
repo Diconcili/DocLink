aba7fae8315ec8eb6ca64be0ae55dc73a72b4969
Eduardo DIconcili <eduardodiconcilicosta@yahoo.com.br>
2026-04-17 15:57:00 -0300

    ProcessPdfCommand implemented

---
+++ DocLinkSolution/Application/Record/Commands/ProcessPdfCommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Record.Commands
+{
+    public sealed class ProcessPdfCommand
+    {
+        public Guid RecordId { get; init; }
+        public string FilePath { get; init; }
+    }
+}
