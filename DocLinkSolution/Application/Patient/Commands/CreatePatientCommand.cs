5b801ca9f70d6c109ed83daf78011a4b393989df
Eduardo DIconcili <eduardodiconcilicosta@yahoo.com.br>
2026-04-17 15:50:23 -0300

    CreatePatientCommand implemented

---
+++ DocLinkSolution/Application/Patient/Commands/CreatePatientCommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Patient.Commands
+{
+    public sealed class CreatePatientCommand
+    {
+        public string FirstName { get; init; }
+        public string LastName { get; init; }
+    }
+}
