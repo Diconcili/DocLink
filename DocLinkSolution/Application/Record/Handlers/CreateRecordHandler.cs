b975d298b8eff6e12f352119c8d4fdfb4b049f09
Eduardo DIconcili <eduardodiconcilicosta@yahoo.com.br>
2026-04-17 16:02:57 -0300

    CreateRecordHandler implemented

---
+++ DocLinkSolution/Application/Record/Handlers/CreateRecordHandler.cs
@@ -0,0 +1,35 @@
+﻿using Application.Record.Commands;
+using Application.Record.DTOs;
+using DocLink.Domain.Patient;
+using DocLink.Domain.Record;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Record.Handlers
+{
+    public sealed class CreateRecordHandler
+    {
+        private readonly RecordService _recordService;
+
+        public CreateRecordHandler(RecordService recordService)
+        {
+            _recordService = recordService;
+        }
+
+        public async Task<RecordDto> HandleAsync(CreateRecordCommand command)
+        {
+            var patientId = PatientId.From(command.PatientId);
+
+            var record = await _recordService.CreateAsync(patientId);
+
+            return new RecordDto
+            {
+                Id = record.Id.Value,
+                PatientId = record.PatientId.Value,
+                Documents = Enumerable.Empty<DocumentDto>()
+            };
+
+        }
+    }
+}
